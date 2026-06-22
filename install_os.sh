fea85c847735c0c21c325898c9fa4eea19135e1a
RichardMJaks <richardmiikael.jaks@gmail.com>
2026-06-22 03:55:56 +0300

    feat(install_os.sh): create os install script

---
+++ install_os.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# vim: fdm=marker fmr={{{,}}}
+
+echo "Dotfiles can be created by either replacing files with the ones in this repository, or by creating symlinks."
+echo "Hard replacement allows you to create your own settings, and keep your modifications."
+echo "With symlinks all your changes will be linked to the ones in this repository, and any changes made will be overwritten after every pull from the repository."
+printf "Create symlinks instead of replacement? [y/N]: "
+read create_symlinks
+create_symlinks=$(create_symlinks,,}
+
+backupAndReplace() {
+	local target_file="$1"
+	local replacement_file="$2"
+	# Validate args
+	if [ -z "$target_file" ] || [ -z "replacement_file" ]; then
+		echo "Usage: backupAndReplace target_file replacement_file"
+		return 1
+	fi
+	
+	echo "Replacing $target_file..."
+	# Create backup if file exists
+	if [ -e "$target_file" ]; then
+		mv "$target_file" "$target_file".bkp
+	fi
+
+	# Replace the file
+	if [ create_symlinks -eq "y" ]; then
+		ln -sr "$replacement_file" "$target_file"
+	else
+		cp "$replacement_file" "$target_file"
+	fi
+}
+echo "Installing packages..."
+read sync_packages < sync_packages.txt
+read aur_packages < aur_packages.txt
+
+# Install required packages
+sudo pacman -S sync_packages
+sudo paru -S aur_packages
+
+echo "Enabling wheel group in /etc/sudoers..."
+# Give sudo permissions to wheel group
+sudo sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL' /etc/sudoers
+
+echo "Creating user..."
+#{{{ User creation
+printf "Enter your username: "
+read username
+
+password_initial="1"
+password_after="0"
+while
+	printf "Enter your password: "
+	read -s password_initial
+
+	printf "Repeat your password: "
+	read -s password_after
+	[[ "$password_initial" != "$password_after" ]]
+do true; done
+
+# Initialize the user with some basic groups
+useradd -m -G games,video,storage,kvm,input,audio,wheel -P "$password_initial" "$username"
+#}}}
+
+homedir=$(eval echo ~"$username")
+
+#{{{ ZSH Configuration
+echo "Setting zsh as default shell for the user..."
+runuser -l $username -c 'chsh -s $(which zsh)' # Set zsh as default shell for user
+# Install oh-my-zsh
+echo "Installing oh-my-zsh..."
+runuser -l "$username" -c 'sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+backupAndReplace "$homedir"/.zshrc zsh/zshrc
+
+# Starship prompt
+backupAndReplace "$homedir"/.config/starship.toml starship/starship.toml
+#}}}
+
+# Niri & Noctalia Configuration
+backupAndReplace "$homedir"/.config/noctalia noctalia
+backupAndReplace "$homedir"/.config/niri niri 
+
+# GTK and Qt configuration
+backupAndReplace "$homedir"/.config/gtk-3.0 gtk/gtk-3.0
+backupAndReplace "$homedir"/.config/nwg-look/config nwg-look/config
+backupAndReplace "$homedir"/.config/qt5ct qt/qt5ct
+backupAndReplace "$homedir"/.config/qt6ct qt/qt6ct
+
+# Kitty Configuration
+backupAndReplace "$homedir"/.config/kitty/kitty.conf kitty/kitty.conf
+
+# Vesktop Configuration
+backupAndReplace "$homedir"/.config/vesktop/themes/noctalia.theme.css vesktop/noctalia.theme.css
+
+# Dolphin Configuration
+backupAndReplace "$homedir"/.config/dolphinrc dolphin/dolphinrc
+
+# Steam Configuration
+backupAndReplace "$homedir"/.local/share/Steam/steamui/skins/Material-Theme/css/main/colors/matugen.css steam/matugen.css
