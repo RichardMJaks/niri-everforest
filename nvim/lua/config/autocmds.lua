c01607ae713327196c9e7ddd4fab99305478f219
Richard Miikael Jaks <richardmiikael.jaks@gmail.com>
2026-06-24 05:45:36 +0300

    feat(nvim): add godot-specific configuration

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,27 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Start Godot LSP
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gdscript",
+    callback = function()
+        local root = vim.fs.dirname(vim.fs.find({ "project.godot" }, { upward = true })[1] or "")
+        if root == "" or root == nil then
+            return -- not a Godot project, don't attach
+        end
+        vim.lsp.start({
+            name = "godot",
+            cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+            root_dir = root,
+        })
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gdscript",
+    callback = function()
+        vim.opt_local.foldmethod = "indent"
+        vim.opt.foldlevelstart = 0
+    end,
+})
